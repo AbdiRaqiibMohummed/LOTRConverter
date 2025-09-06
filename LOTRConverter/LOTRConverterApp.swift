793ad294e384c87385819f4653330e35d54ea3d3
AbdiRaqiibMohummed <aclipz0121@gmail.com>
2025-09-06 17:21:17 +0100

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Abdiraqiib Hassan on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
