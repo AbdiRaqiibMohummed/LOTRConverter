6bf0de299312e7b4ec14c8d361ed3bc2b922df3e
AbdiRaqiibMohummed <aclipz0121@gmail.com>
2025-09-09 21:48:36 +0100

    Feat: finished designing the exchange rate page now learning diffrent ways of extracting and reusing views

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     @State var showExchangeInfo = false
     @State var leftAmount = ""
     @State var rightAmount = ""
-    
+
     var body: some View {
         ZStack {
             // Background image
@@ -23,57 +23,56 @@ struct ContentView: View {
                 Image(.prancingpony)
                     .resizable()
                     .scaledToFit()
-                    .frame(height:200)
-                
+                    .frame(height: 200)
+
                 // currency exchange text
                 Text("Currency Exchange")
                     .font(.largeTitle)
                     .foregroundStyle(.white)
-                
+
                 // conversion section
                 HStack {
                     // left conversion section
                     VStack {
                         //Currency
                         HStack {
-                            
+
                             // Currency image
                             Image(.silverpiece)
                                 .resizable()
                                 .scaledToFit()
                                 .frame(height: 33)
-                            
+
                             // Currency text
                             Text("Silver Piece")
                                 .font(.headline)
                                 .foregroundStyle(.white)
                         }
                         .padding(.bottom, -5)
-                        
+
                         //Text field
-                        
+
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
-                            
-                
+
                     }
                     // Equal sign
                     Image(systemName: "equal")
                         .font(.largeTitle)
                         .foregroundStyle(.white)
                         .symbolEffect(.pulse)
-                    
+
                     // Right Conversion sign
-                    
+
                     VStack {
                         //Currency
                         HStack {
-                            
+
                             // Currency text
                             Text("Gold Piece")
                                 .font(.headline)
                                 .foregroundStyle(.white)
-                            
+
                             // Currency image
                             Image(.goldpiece)
                                 .resizable()
@@ -81,7 +80,7 @@ struct ContentView: View {
                                 .frame(height: 33)
                         }
                         .padding(.bottom, -5)
-                        
+
                         //Text field
                         TextField("Amount", text: $rightAmount)
                             .textFieldStyle(.roundedBorder)
@@ -91,30 +90,30 @@ struct ContentView: View {
                 .padding()
                 .background(.black.opacity(0.5))
                 .clipShape(.capsule)
-                
+
                 Spacer()
-                
+
                 // info button
                 HStack {
                     Spacer()
                     Button {
                         showExchangeInfo.toggle()
-                        
+
                     } label: {
                         Image(systemName: "info.circle.fill")
                             .font(.largeTitle)
                             .foregroundStyle(.white)
                     }
                     .padding(.trailing)
-                    
+
                 }
 
             }
-            
-//            .border(.blue)
-          
+
+            //            .border(.blue)
+
         }
-        .sheet(isPresented: $showExchangeInfo){ // the sheet can be placed anywhere prefably top stack in this case Zstack
+        .sheet(isPresented: $showExchangeInfo) {  // the sheet can be placed anywhere prefably top stack in this case Zstack
             ExchangeInfo()
         }
     }
---
+++ LOTRConverter/SelectCurrency.swift
@@ -0,0 +1,56 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter
+//
+//  Created by Abdiraqiib Hassan on 08/09/2025.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+    
+   
+    var body: some View {
+        ZStack{
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            VStack{
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+                  
+                //Currency Icons
+                HStack{
+                    CurrencyIcon(currencyImage: .copperpenny, currencyName: "Copper Penny")
+                  
+                }
+                
+                // Text
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(.bold)
+            
+                
+                // Done Button
+
+                Button("Done") {
+                   dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .padding()
+
+            }
+            .padding()
+            .multilineTextAlignment(.center) // this basically centers the text in the whole vstack
+        }
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}
+
