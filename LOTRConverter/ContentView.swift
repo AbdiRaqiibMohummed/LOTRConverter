9e6815c671b4511b9c9c370cb703960404b2875f
AbdiRaqiibMohummed <aclipz0121@gmail.com>
2025-09-17 15:50:33 +0100

    Feat: added a convert function in Currency.swift which based on the value of the field will convert to the field opposite Currently learnt onChange and now learning @FocusState

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     @State var leftAmount = ""
     @State var rightAmount = ""
     
+    @FocusState var leftTyping
+    @FocusState var rightTyping
+    
     @State var leftCurrency: Currency = .silverPiece
     @State var rightCurrency: Currency = .goldPiece
     
@@ -62,6 +65,9 @@ struct ContentView: View {
 
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
+                            .onChange(of: leftAmount) {
+                                rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+                            }
                          
 
                     }
@@ -97,6 +103,9 @@ struct ContentView: View {
                         TextField("Amount", text: $rightAmount)
                             .textFieldStyle(.roundedBorder)
                             .multilineTextAlignment(.trailing)
+                            .onChange(of: rightAmount){
+                                leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+                            }
                     }
                 }
                 .padding()
